77a32efc881d8d52f4fbf641166375a74c64915a
artyom <artyom.karpets@softeq.com>
2022-10-03 17:02:55 +0600

    Add controller

---
+++ src/UserAnswers/TrialsSystem.QuestionnairesService/TrialsSystem.QuestionnairesService.Api/Controllers/v1/QuestionnairesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrialsSystem.QuestionnairesService.Api.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class QuestionnairesController : ControllerBase
+    {
+    }
+}
